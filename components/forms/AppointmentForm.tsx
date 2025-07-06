27a25b3b3050b45398ba3498591d17b48c8e793d
HoangPhiHungGG <20H1160124@ut.edu.vn>
2025-07-06 23:42:53 +0700

    update sửa lỗi

--- components/forms/AppointmentForm.tsx
+++ components/forms/AppointmentForm.tsx
@@ -101,6 +101,8 @@ const AppointmentForm = ({
             cancellationReason: values.cancellationReason,
           },
           type,
+              timeZone: Intl.DateTimeFormat().resolvedOptions().timeZone,
+
         };
 
         const updatedAppointment = await updateAppointment(appointmentToUpdate);
